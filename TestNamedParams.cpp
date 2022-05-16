0699688106a2d49898864bc141f1b953f0a02415
ambmax00 <maximilien.ambroise@gmail.com>
2022-05-16 21:09:55 +0200

    Add test for static class function

--- TestNamedParams.cpp
+++ TestNamedParams.cpp
@@ -9,6 +9,13 @@
     _RETURN += 1; \
   } 
 
+#define CHECK_ALMOST_EQUAL(_A, _B, _RETURN) \
+  if (!(std::abs(_A - _B) < 1e-6)) \
+  { \
+    std::cerr << "Not equal: " << #_A << " " << #_B << std::endl; \
+    _RETURN += 1; \
+  } 
+
 #define UNPAREN(...) __VA_ARGS__ 
 #define KEY(TYPE, ID) inline static const Key< UNPAREN TYPE , ID > 
 #define KEYOPT(TYPE, ID) inline static const Key<std::optional< UNPAREN TYPE >, ID >
@@ -42,6 +49,58 @@ std::string foo3(std::optional<std::string> s)
 KEYOPT((std::string),5) key3;
 KEYGEN foo3Wrapper(&foo3, key3);
 
+class Test
+{
+  public:
+
+    int m_int;
+    float m_float;
+    std::string m_str;
+
+    KEY((int), 0) param0;
+    KEY((float), 1) param1;
+    KEYOPT((std::string),2) param2;
+
+    KEY((int), 3) param3;
+    KEY((int), 4) param4;
+    KEY((float), 5) param5;
+    KEYOPT((int), 6) param6;
+
+    Test(int _i, float _f, std::string _s) 
+      : m_int(_i)
+      , m_float(_f)
+      , m_str(_s)
+    {
+    }
+  
+    static Test build(int _i, float _f, std::optional<std::string> _strOpt)
+    {
+      if (_strOpt)
+      {
+        return Test(_i, _f, *_strOpt);
+      }
+      else 
+      {
+        return Test(_i, _f, "");
+      }
+    }
+
+    KEYGEN buildWrapper = KeyGen(&Test::build, param0, param1, param2);
+
+    int compute(int _a, int _b, float _c, std::optional<int> _d)
+    {
+      if (_d)
+      {
+        return m_int + _a + _b + _c + *_d;
+      }
+      else 
+      {
+        return m_int + _a + _b + _c;
+      }
+    }
+
+};
+
 int main(int argc, char** argv)
 {
   int result = 0;
@@ -64,6 +123,18 @@ int main(int argc, char** argv)
   std::string ret4 = foo3Wrapper();
   CHECK_EQUAL(ret4, "", result);
   
+  Test t0 = Test::buildWrapper(Test::param1 = 3.14, Test::param2 = "HELLO", Test::param0 = 1);
+  CHECK_EQUAL(t0.m_int, 1, result);
+  CHECK_ALMOST_EQUAL(t0.m_float, 3.14, result);
+  CHECK_EQUAL(t0.m_str, "HELLO", result);
+
+  /*Test t1 = Test::buildWrapper(Test::param0 = 5, Test::param1 = 6.28); // TAU GANG REPRESENT
+  CHECK_EQUAL(t1.m_int, 5, result);
+  CHECK_ALMOST_EQUAL(t1.m_float, 6.28, result);
+  CHECK_EQUAL(t0.m_str, "", result);*/
+
+  // FIX SAME KEY
+
   return result;
 
 }
